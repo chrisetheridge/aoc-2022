6f871920de1886986d56d06d1554b289516461ca
Chris Etheridge <chrise@cognician.com>
2022-12-05 09:14:21 +0200

    Day 4

---
+++ src/aoc_2022/day4.clj
@@ -0,0 +1,45 @@
+(ns aoc-2022.day4
+  (:require [aoc-2022.core :as a22]
+            [clojure.string :as string]
+            [clojure.set :as set]))
+
+(defn parse-to-ranges [pair]
+  (let [[start end] (string/split pair #"-")]
+    (range (parse-long start)
+           (inc (parse-long end)))))
+
+(defn explode-pairs [s]
+  (->> (string/split s #",")
+       (map parse-to-ranges)
+       (map set)))
+
+(def input
+  (->> (a22/read-input 4)
+       (map explode-pairs)))
+
+(defn pairs-cover? [p1 p2]
+  (or (set/subset? p1 p2)
+      (set/superset? p1 p2)))
+
+(def sample
+  (->> "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
+       string/split-lines
+       (map explode-pairs)))
+
+(defn part-1 [_]
+  (->> input
+       (map (fn [[p1 p2]]
+              (pairs-cover? p1 p2)))
+       (remove false?)
+       count))
+
+(defn part-2 [_]
+  (->> input
+       (keep (fn [[p1 p2]]
+               (seq (set/intersection p1 p2))))
+       count))
