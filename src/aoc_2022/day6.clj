959de728a4275b9d61fab0d0ccea6fc375657d8e
Chris Etheridge <chrise@cognician.com>
2022-12-06 08:34:15 +0200

    Day 6

---
+++ src/aoc_2022/day6.clj
@@ -0,0 +1,30 @@
+(ns aoc-2022.day6
+  (:require [aoc-2022.core :as a22.core]))
+
+(def input
+  (a22.core/read-input 6))
+
+(def samples
+  [["bvwbjplbgvbhsrlpgdmjqwftvncz" 5]
+   ["nppdvjthqldpwncqszvftbrmjlhg" 6]
+   ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 10]
+   ["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 11]])
+
+(defn find-marker-position [n chars]
+  (loop [pos 0
+         chars chars]
+    (let [probe (take n chars)]
+      (if (= n (count (set probe)))
+        [probe (+ n  pos)]
+        (recur (inc pos)
+               (next chars))))))
+
+(defn part-one [_]
+  (last (find-marker-position 4 input)))
+
+(defn part-two [_]
+  (last (find-marker-position 14 input)))
+
+(defn run [_]
+  (println "Part one:" (a22.core/timed (part-one nil)))
+  (println "Part two:" (a22.core/timed (part-two nil))))
